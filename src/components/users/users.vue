c317d6af2aac40ea9f1d026885553a5884c0d3c5
FXF33 <543126977@qq.com>
2018-11-27 21:36:42 +0800

    删除用户信息实现

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -1,57 +1,62 @@
 <template>
-<el-card class="box-card">
+  <el-card class="box-card">
     <el-breadcrumb separator-class="el-icon-arrow-right">
-        <el-breadcrumb-item>首页</el-breadcrumb-item>
-        <el-breadcrumb-item>用户管理</el-breadcrumb-item>
-        <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+      <el-breadcrumb-item>首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
 
     <el-row class="searchRow">
-        <el-col :span="16">
-            <el-input @clear="loadUserList()" clearable v-model="query" placeholder="请输入用户名" class="searchInput">
-                <el-button @click="searchUser()" slot="append" icon="el-icon-search"></el-button>
-            </el-input>
-            <el-button type="success" @click="showAddUserDia()">添加用户</el-button>
-        </el-col>
+      <el-col :span="16">
+        <el-input
+          @clear="loadUserList()"
+          clearable
+          v-model="query"
+          placeholder="请输入用户名"
+          class="searchInput"
+        >
+          <el-button @click="searchUser()" slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-button type="success" @click="showAddUserDia()">添加用户</el-button>
+      </el-col>
     </el-row>
 
     <el-table :data="userlist" style="width: 100%">
-        <el-table-column type="index" label="#" width="80"></el-table-column>
-        <el-table-column prop="username" label="姓名" width="100"></el-table-column>
-        <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
-        <el-table-column prop="mobile" label="电话"></el-table-column>
-        <el-table-column label="创建日期">
-            <template slot-scope="scope">{{scope.row.create_time |fmtdate}}</template>
-        </el-table-column>
+      <el-table-column type="index" label="#" width="80"></el-table-column>
+      <el-table-column prop="username" label="姓名" width="100"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
+      <el-table-column prop="mobile" label="电话"></el-table-column>
+      <el-table-column label="创建日期">
+        <template slot-scope="scope">{{scope.row.create_time |fmtdate}}</template>
+      </el-table-column>
 
-        <el-table-column label="用户状态">
-            <template slot-scope="scope">
-                <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949">
-                </el-switch>
-            </template>
+      <el-table-column label="用户状态">
+        <template slot-scope="scope">
+          <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
+      </el-table-column>
 
-        </el-table-column>
-
-        <el-table-column label="操作">
-            <template slot-scope="scope">
-                <el-button
-                @click="showEditUserDia()"
-                size="mini"
-                plain
-                type="primary"
-                icon="el-icon-edit"
-                circle></el-button>
-                <el-button
-                @click="showMegBoxDele(scope.row.id)"
-                size="mini"
-                plain
-                type="danger"
-                icon="el-icon-delete"
-                circle></el-button>
-                <el-button size="mini" plain type="success" icon="el-icon-check" circle></el-button>
-
-            </template>
-        </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            @click="showEditUserDia()"
+            size="mini"
+            plain
+            type="primary"
+            icon="el-icon-edit"
+            circle
+          ></el-button>
+          <el-button
+            @click="showMegBoxDele(scope.row.id)"
+            size="mini"
+            plain
+            type="danger"
+            icon="el-icon-delete"
+            circle
+          ></el-button>
+          <el-button size="mini" plain type="success" icon="el-icon-check" circle></el-button>
+        </template>
+      </el-table-column>
     </el-table>
 
     <el-pagination
@@ -61,88 +66,149 @@
       :page-sizes="[2, 4, 6, 8]"
       :page-size="2"
       layout="total, sizes, prev, pager, next, jumper"
-      :total="total">
-    </el-pagination>
-</el-card>
+      :total="total"
+    ></el-pagination>
+
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisibleAdd">
+      <el-form :model="form">
+        <el-form-item label="用户名称" label-width="100px">
+          <el-input v-model="form.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" label-width="100px">
+          <el-input v-model="form.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="100px">
+          <el-input v-model="form.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" label-width="100px">
+          <el-input v-model="form.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
+        <el-button type="primary" @click="addUser()">确 定</el-button>
+      </div>
+    </el-dialog>
+  </el-card>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            query: '',
-            userlist: [{
-                create_time:'',
-                email:'',
-                id:'',
-                mg_state:'', 
-                mobile:'',
-                role_name:'',
-                username:''
-            }],
-            pagenum: 1,
-            pagesize: 2,
-            total: -1
+  data() {
+    return {
+      query: "",
+      userlist: [
+        {
+          create_time: "",
+          email: "",
+          id: "",
+          mg_state: "",
+          mobile: "",
+          role_name: "",
+          username: ""
         }
+      ],
+      pagenum: 1,
+      pagesize: 2,
+      total: -1,
+      dialogFormVisibleAdd: false,
+      form: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
+      }
+    };
+  },
+  created() {
+    this.getUserList();
+  },
+  methods: {
+    showMegBoxDele(userId) {
+      this.$confirm("是否删除用户", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(async () => {
+        const res = await this.$http.delete(`users/${userId}`);
+        this.getUserList();
+        this.$message({
+          type: "success",
+          message: res.data.meta.msg
+        });
+      });
     },
-    created() {
-        this.getUserList()
+
+    async addUser() {
+      this.dialogFormVisibleAdd = false;
+      const res = await this.$http.post("users", this.form);
+      const {
+        meta: { status, msg },
+        data
+      } = res.data;
+      if (status === 201) {
+        this.$message.success(msg);
+        this.getUserList();
+        this.form = {};
+      } else {
+        this.$message.warning(msg);
+      }
     },
-    methods: {
-        loadUserList(){
-            this.getUserList()
-        },
-        searchUser(){
-            this.pagenum=1
-            this.getUserList()
-        },
-        handleSizeChange(val){
-            this.pagesize=val
-            this.pagenum=1
-            this.getUserList()
-        },
-        handleCurrentChange(val){
-            // console.log(`当前页: ${val}`)
-            this.pagenum=val
-            this.getUserList()
-        },
-        async getUserList() {
-            const AUTH_TOKEN = localStorage.getItem('token')
-            this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
-            const res = await this.$http.get(`users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`)
-            // console.log(res)
-            const {
-                meta: {
-                    msg,
-                    status
-                },
-                data: {
-                    total,
-                    users
-                }
-            } = res.data
-            if (status === 200) {
-                this.total = total
-                this.userlist = users
-                this.$message.success(msg)
-            } else {
-                this.$message.warning(msg)
-            }
-        }
+    showAddUserDia() {
+      this.dialogFormVisibleAdd = true;
+    },
+    loadUserList() {
+      this.getUserList();
+    },
+    searchUser() {
+      this.pagenum = 1;
+      this.getUserList();
+    },
+    handleSizeChange(val) {
+      this.pagesize = val;
+      this.pagenum = 1;
+      this.getUserList();
+    },
+    handleCurrentChange(val) {
+      // console.log(`当前页: ${val}`)
+      this.pagenum = val;
+      this.getUserList();
+    },
+    async getUserList() {
+      const AUTH_TOKEN = localStorage.getItem("token");
+      this.$http.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+      const res = await this.$http.get(
+        `users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${
+          this.pagesize
+        }`
+      );
+      // console.log(res)
+      const {
+        meta: { msg, status },
+        data: { total, users }
+      } = res.data;
+      if (status === 200) {
+        this.total = total;
+        this.userlist = users;
+        this.$message.success(msg);
+      } else {
+        this.$message.warning(msg);
+      }
     }
-}
+  }
+};
 </script>
 
 <style>
 .box-card {
-    height: 100%;
+  height: 100%;
 }
 
 .searchRow {
-    margin-top: 20px;
+  margin-top: 20px;
 }
 
 .searchInput {
-    width: 300px;
+  width: 300px;
 }
 </style>
