1a80cc04235e49a5dfc447865b1c6101265ee717
FXF33 <543126977@qq.com>
2018-11-28 11:58:49 +0800

    eslint取消和清除localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <div id="app">
+<div id="app">
 
-    <router-view/>
-  </div>
+    <router-view />
+</div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  
-  // mounted(){
-  //       // 关闭浏览器窗口的时候清空浏览器缓存在localStorage的数据
-  //       window.onbeforeunload = function (e) {
-  //           var storage = window.localStorage;
-  //           storage.clear()
-  //       }
-  // }
+    name: 'App',
+    mounted() {
+        // 关闭浏览器窗口的时候清空浏览器缓存在localStorage的数据
+        window.onbeforeunload = function (e) {
+            var storage = window.localStorage;
+            storage.clear()
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  height: 100%;
+    height: 100%;
 }
 </style>
